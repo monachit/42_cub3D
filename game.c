5ce2af1341351c821b363d539306fee789e74f74
Mouad Nachit <mnachit@e1r3p10.1337.ma>
2024-11-12 12:17:47 +0100

    stay

--- game.c
+++ game.c
@@ -76,6 +76,26 @@ void draw_line(t_vars *vars, double start_x, double start_y, double end_x, doubl
     }
 }
 
+int	is_ray_facing_down(double rayAngle)
+{
+	return (rayAngle > 0 && rayAngle < M_PI);
+}
+
+int	is_ray_facing_up(double rayAngle)
+{
+	return (!is_ray_facing_down(rayAngle));
+}
+
+int	is_ray_facing_right(double rayAngle)
+{
+	return (rayAngle < 0.5 * M_PI || rayAngle > 1.5 * M_PI);
+}
+
+int	is_ray_facing_left(double rayAngle)
+{
+	return (!is_ray_facing_right(rayAngle));
+}
+
 double nor_angle(double angle)
 {
 	if (angle < 0)
@@ -104,53 +124,33 @@ int    wall_check(double x, double y, t_vars *vars)
 {
     int mx, my;
 
-    mx = (int)x / TILE_SIZE;
-    my = (int) y / TILE_SIZE;
+    mx = (int)(x / TILE_SIZE);
+    my = (int)(y / TILE_SIZE);
     
-    if (mx < 0 || my < 0 || mx >= vars->win_width || my >= vars->win_height )
+    if (mx < 0 || my < 0 || mx >= vars->win_width || my >= vars->win_height)
         return (0);
      if  (vars->map1[my][mx] == '1')
         return (0);
     return (1);
 }
-int inter_check(double angle, double *inter, double *step, int is_horizon)
-{
-	if (is_horizon)
-	{
-		if (angle > 0 && angle < M_PI)
-		{
-			*inter += TILE_SIZE;
-			return (-1);
-		}
-		*step *= -1;
-	}
-	else
-	{
-		if (!(angle > M_PI / 2 && angle < 3 * M_PI / 2)) 
-		{
-			*inter += TILE_SIZE;
-			return (-1);
-		}
-		*step *= -1;
-	}
-	return (1);
-}
+
 double get_h_inter(t_vars *vars, double angl)
 {
     double h_x, h_y;
     double x_step, y_step;
 
-    if (angl > 0 && angl < M_PI)
+    y_step = TILE_SIZE;
+    if (is_ray_facing_up(angl))
+		y_step *= -1;
+    x_step = y_step / tan(angl);
+    if ((is_ray_facing_left(angl) && x_step > 0) || (is_ray_facing_right(angl)
+		&& x_step < 0))
+		x_step *= -1;
+    if (is_ray_facing_down(angl))
         h_y = floor(vars->p_y / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
     else 
-        h_y = floor(vars->p_y / TILE_SIZE) * TILE_SIZE;
-    
+        h_y = floor(vars->p_y / TILE_SIZE) * TILE_SIZE - 0.001;
     h_x = vars->p_x + (h_y - vars->p_y) / tan(angl);
-    if (angl > 0 && angl < M_PI)
-        y_step = TILE_SIZE;
-    else
-        y_step = -TILE_SIZE;
-    x_step = y_step / tan(angl);
     while (wall_check(h_x, h_y, vars))
     {
         h_x += x_step;
@@ -165,16 +165,18 @@ double get_v_inter(t_vars *vars, double angl)
     double v_x, v_y;
     double x_step, y_step;
 
-    if (angl < M_PI_2 || angl > 3 * M_PI_2)
-        v_x = floor(vars->p_x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
-    else
-        v_x = floor(vars->p_x / TILE_SIZE) * TILE_SIZE;
-    v_y = vars->p_y + (v_x - vars->p_x) * tan(angl);
-    if (angl < M_PI_2 || angl > 3 * M_PI_2)
-        x_step = TILE_SIZE;
-    else 
-        x_step = -TILE_SIZE;
+    x_step = TILE_SIZE;
+    if (is_ray_facing_left(angl))
+		x_step *= -1;
     y_step = x_step * tan(angl);
+    if ((is_ray_facing_up(angl) && y_step > 0 ) || (is_ray_facing_down(angl)
+			&& y_step < 0))
+		y_step *= -1;
+	if (is_ray_facing_left(angl))
+		v_x = floor(vars->p_x / TILE_SIZE) * TILE_SIZE - 0.001;
+	else
+		v_x = floor(vars->p_x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
+    v_y = vars->p_y + (v_x - vars->p_x) * tan(angl);
     while (wall_check(v_x, v_y, vars))
     {
         v_x += x_step;
@@ -183,24 +185,24 @@ double get_v_inter(t_vars *vars, double angl)
     return sqrt(pow(v_x - vars->p_x, 2) + pow(v_y - vars->p_y, 2));
 }
 
-
 void rander_wall(t_vars *vars, int ray, double ray_a)
 {
     double wall_h;
     double bottom_p;
     double top_p;
 
-    // vars->distence *= cos(nor_angle(ray_a - M_PI));
-    wall_h = (TILE_SIZE / vars->distence) * (vars->win_width * 100/ 2) / tan(FOV / 2);
-    top_p = (900 / 2) - (wall_h /2);
-    bottom_p = (900 / 2) + (wall_h /2);
+    vars->distence *= cos(ray_a - vars->direction);
+ 
+    wall_h = (TILE_SIZE / vars->distence) * (vars->win_width * TILE_SIZE / 2) / tan(FOV / 2);
+    top_p = (vars->win_height * 100 / 2) - (wall_h /2);
+    bottom_p = (vars->win_height * 100 / 2) + (wall_h /2);
 
     if (top_p < 0)
         top_p = 0;
     if (bottom_p > 900)
         bottom_p = 900;
-    while (top_p < bottom_p)
-        my_mlx_pixel_put(vars, ray, top_p++, 0xFFFFFF);
+    while (top_p <= bottom_p)
+        my_mlx_pixel_put(vars, ray, (int)top_p++, 0xFFFFFF);
 }
 
 void cast_rays(t_vars *vars)
@@ -218,22 +220,23 @@ void cast_rays(t_vars *vars)
     {
         x_inter = get_v_inter(vars, nor_angle(first_ray)); // vertical
         h_inter = get_h_inter(vars, nor_angle(first_ray)); // horizontal 
-        if (x_inter <= h_inter)
+        if (x_inter < h_inter)
             vars->distence = x_inter;
         else
             vars->distence = h_inter;
-        // draw_line(vars, vars->p_x, vars->p_y, vars->p_x + cos(first_ray) * vars->distence, vars->p_y + sin(first_ray) * vars->distence, 0xFFFFFF);
-        first_ray += add_angl;
+        // printf("%f \n",vars->distence);
+        // draw_line(vars, vars->p_x, vars->p_y, vars->p_x + cos(first_ray) * vars->distence, vars->p_y + sin(first_ray) * vars->distence, 5646546);
         rander_wall(vars, ray, first_ray);
+        first_ray += add_angl;
         ray++;
     }
-
+    
 }
 
 void drawing(t_vars *vars)
 {    
     clear_image(vars, 0x000000); 
-    // // draw_square(vars, vars->p_x, vars->p_y, 20, 0xFF6FFF);
+    // draw_square(vars, vars->p_x, vars->p_y, 20, 0xFF6FFF);
     // for (int i = 0; i < vars->win_height; i++) 
     // {
     //     for (int j = 0; j < vars->win_width; j++) 
@@ -250,21 +253,29 @@ int key_hook(int keycode, t_vars *vars)
 {
     double move_speed = 20.0;
     double rotation_speed = 0.1;
+    double new_x, new_y;
+
+    new_x = vars->p_x;
+    new_y = vars->p_y;
     if (keycode == 13)  // Up (W key) 113
     {
-        vars->p_x += cos(vars->direction) * move_speed;
-        vars->p_y += sin(vars->direction) * move_speed;
+        new_x += cos(vars->direction) * move_speed;
+        new_y += sin(vars->direction) * move_speed;
     } 
     else if (keycode == 1)  // Down (S key) 115
     {
-        vars->p_x -= cos(vars->direction) * move_speed;
-        vars->p_y -= sin(vars->direction) * move_speed;
+        new_x -= cos(vars->direction) * move_speed;
+        new_y -= sin(vars->direction) * move_speed;
     } 
     else if (keycode == 123)  // Left (A key) 97
         vars->direction -= rotation_speed;
     else if (keycode == 124)  // Right (D key) 100 on linux
         vars->direction += rotation_speed;
-
+    if (wall_check(new_x, new_y, vars))
+    {
+        vars->p_x = new_x;
+        vars->p_y = new_y;
+    }
     // draw_square(vars, vars->p_x, vars->p_y, 20, 0x000000);
     vars->direction = nor_angle(vars->direction);
     drawing(vars);
@@ -292,6 +303,12 @@ void    set_pos(char **map, int *a, int *b)
         }
         i++;
     }
+}
+int	close_window(void *v)
+{
+    (void)v;
+	exit(EXIT_SUCCESS);
+	return (0);
 }
 
 void    game_plan(t_data *data)
@@ -337,6 +354,7 @@ void    game_plan(t_data *data)
     mlx_hook(vars.win, 2, 1L<<0, key_hook, &vars);
     mlx_put_image_to_window(vars.mlx, vars.win, vars.img, 0, 0);
     
+	mlx_hook(vars.win, 17, 0L, close_window, NULL);
     mlx_loop(vars.mlx);
 
 }
